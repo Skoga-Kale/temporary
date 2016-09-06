ab5648def4b394c436f81be40cc6552ab561afc6
Skoga <kale.thompson@utah.edu>
2016-09-06 13:53:32 -0600

    This is my final version. Extensive testing and comments included.

--- PS1/Evaluator Tester/Tester.cs
+++ PS1/Evaluator Tester/Tester.cs
@@ -13,7 +13,7 @@ namespace Evaluator_Tester
         {
             testDictionary.Add("B1B1", 0);
             testDictionary.Add("CC123", 4);
-            int answer = FormulaEvaluator.Evaluator.Evaluate("5 + 3 * 7 - 8 / (CC123 + 3) - 2 / 2", TestDelegate);
+            int answer = FormulaEvaluator.Evaluator.Evaluate("5 * (5 * 7) * 6 * 5", TestDelegate);
             Console.WriteLine(answer);
             Console.Read();
         }
